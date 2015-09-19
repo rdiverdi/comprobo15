7227948baa8c3460f61fa627e206adbb305c98fe
rdiverdi <Rocco.DiVerdi@students.olin.edu>
2015-09-19 13:02:29 -0400

    more comments

--- warmup_project/nodes/States.py
+++ warmup_project/nodes/States.py
@@ -94,63 +94,68 @@ class Robot_Control():
         self.state='follow_wall' #change state to 'follow wall'
 
     def find_things(self):
+        '''Sweeps the area in .2-1.5 meters of the Neato with the lidar, and calculates the center of mass of the points it sees during find person
+         -returns a tuple of the angle of the COM relative to the front of the robot and the distance of the COMfrom the robot for find person
+         -returns a list of things it saw in its range during obstacle avoid'''
         while self.data == ():
             pass
         if self.state == 'obstacle_avoid':
-            pad = 179
-            max_dist = 1.25
+            pad = 179 #the angle the robot sweeps to on either side, starting from the front
+            max_dist = 1.25 #max of desired distacne range
         elif self.state == 'follow_person':
-            pad = 35
+            pad = 35 #the angle the robot sweeps to on either side, starting from the front
             max_dist = 1.5
-        readings = []
-        for i in range(2*pad):
-            index = (360-pad+i)%360
-            dist = self.data[index]
-            readings.append(dist)
-        variable_name = list(enumerate(readings))
-        body_reading = []
-        for reading in variable_name:
-            if reading[1] > .25 and reading[1] < max_dist:
+        readings = [] #array of lidar readings 
+        for i in range(2*pad): #sweeps through the angles defined by pad
+            index = (360-pad+i)%360     #calculates actual angle with respect to the robot's coordinate system
+            dist = self.data[index]     #reads values from those angles
+            readings.append(dist)       #adds to readings array
+        variable_name = list(enumerate(readings))   #turns readings into a list of tuples containing their index and data value
+        body_reading = []   #reading of points that define what the robot should follow
+        for reading in variable_name:   #sweeps through readings from enumerated list
+            if reading[1] > .25 and reading[1] < max_dist:    #adds all readings inside a specified box to the body_reading list
                 body_reading.append(reading)
-        angle_sum=0.0
-        dist_sum=0.0
-        for point in body_reading:
-            angle_sum+=point[0]
-            dist_sum+=point[1]
+        angle_sum=0.0   #sum of all angles for COM calculation
+        dist_sum=0.0    #sum of all distances for COM calculation
+        for point in body_reading:  
+            angle_sum+=point[0]     #sums up angles (still enumerations, but because of their nature are just offset angles)
+            dist_sum+=point[1]  #sums up all distances
         if self.state == 'follow_person':
-            if len(body_reading) == 0:
+            if len(body_reading) == 0: #for first instance of no reading, intitialize start_time
                 if self.start_time == 0.0:
                     self.start_time = time.time()
                 else:
-                    if time.time() - self.start_time > 3:
+                    if time.time() - self.start_time > 3: #after 3 seconds of no valid readings, change state to obstacle avoid
                         self.state = 'obstacle_avoid'
-                return (0.0, self.set_dist)
-            else:
+                return (0.0, self.set_dist) #avoid divide by zero
+            else: #if there is a valid reading, reset start_time
                 self.start_time = 0.0
-            avg_angle = angle_sum/len(body_reading)
-            avg_dist = dist_sum/len(body_reading)
-            return (avg_angle-35, avg_dist)
+            avg_angle = angle_sum/len(body_reading) #get average angle
+            avg_dist = dist_sum/len(body_reading) #get average distance
+            return (avg_angle-35, avg_dist) #returns tuple of COM values and accounts for angle offset from enumerate
         if self.state == 'obstacle_avoid':
-            return body_reading
+            return body_reading #for obstacle avoid, just return list of values in the set range
 
     def follow_person(self):
-        (angle, distance) = self.find_things()
-        k= -1.27
-        c= 1.5
-        dist_diff = distance - self.set_dist
-        angle_diff = -angle
-        forward = c*dist_diff
-        spin = k*angle_diff/35
-        if dist_diff > -0.003 and dist_diff < 0.003:
-            #print dist_diff
-            if self.start_time_wall == 0.0:
+        '''Follows a person in front of the robot by implementing proportional control and sends commands to the robot to do so'''
+        (angle, distance) = self.find_things()  #obtains angle and distance of COM
+        k= -1.27   #constant defining how fast the robot will spin
+        c= 1.5     #constant defining how fast the robot will move
+        dist_diff = distance - self.set_dist    #the difference between the set distance to keep from the person and the actual distance
+        angle_diff = -angle     #the angle difference between the person and the robot
+        forward = c*dist_diff   #how fast the robot should move forward to shorten the distance
+        spin = k*angle_diff/35  #how fast the robot should turn to face the person
+        if dist_diff > -0.003 and dist_diff < 0.003: #if the object is at the desired distance
+            if self.start_time_wall == 0.0: 
+                #first time, start a timer
                 print 'start'
                 self.start_time_wall = time.time()
             elif time.time() - self.start_time_wall > 5:
+                #after 5 seconds of the target object staying in the desired range, change state to find wall
                 self.state = 'find_wall'
-                self.start_time_wall = 0.0
+                self.start_time_wall = 0.0 #reset timer
         else:
-            self.start_time = 0.0
+            self.start_time = 0.0 #if the object moves, reset timer
 
         self.set_motion(forward=forward,spin=spin)
 
